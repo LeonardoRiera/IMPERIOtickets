b8748e0347c83e334c2152adaf7b4b0343bceeda
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-11-15 21:05:32 -0300

    nuevo responsive para footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -8,68 +8,61 @@ import logoNegrito from '../../assets/logo0.06.png'
 
 export const Footer = () => {
   return (
-      <div className='footer-container container-fluid' id='footer'>
+    <div className='footer-container container-fluid' id='footer'>
 
-        {/* footer */}
-        <div className='row contenedorFooterPrincipal '>
+      {/* footer */}
+      <div className='row contenedorFooterPrincipal '>
 
+        
+        <div className='col-lg-3 col-md-12 contactoRedesFooter'>
+
+          <img className='logoPalabraFooter' src={logoPalabra} alt="logo de imperio ticket" />  
           
-          <div className='col-lg-3 col-md-12 contactoRedesFooter'>
-
-            <img className='logoPalabraFooter' src={logoPalabra} alt="logo de imperio ticket" />  
-            
-            <div className='contactoFooter'>
-              <h4>Puedes Contactarte con Nosotros <br/> vía e-mail</h4>
-              <a href="mailto:imperioticket@gmail.com">imperiotickets@gmail.com</a>
-            </div>
-
-            <div className="social-icons">
-              {/* Enlace a Instagram */}
-              <a href="https://www.instagram.com/tu_usuario" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-instagram"></i>
-              </a>
-
-              {/* Enlace a X (Twitter) */}
-              <a href="https://twitter.com/tu_usuario" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-twitter"></i> {/* X usa el ícono de Twitter */}
-              </a>
-
-              {/* Enlace a Discord */}
-              <a href="https://discord.com/invite/tu_invitacion" target="_blank" rel="noopener noreferrer">
-                <i className="bi bi-discord"></i>
-              </a>
-            </div>
-
+          <div className='contactoFooter'>
+            <h4>Puedes Contactarte con Nosotros <br/> vía e-mail</h4>
+            <a href="mailto:imperioticket@gmail.com">imperiotickets@gmail.com</a>
           </div>
 
-          <div className='col-lg-6 col-md-12 colCentralFooter'>
-            
-            <img className='planetaFooter' src={planeta} alt="" />  
-            
+          <div className="social-icons">
+            {/* Enlace a Instagram */}
+            <a href="https://www.instagram.com/tu_usuario" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-instagram"></i>
+            </a>
+
+            {/* Enlace a X (Twitter) */}
+            <a href="https://twitter.com/tu_usuario" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-twitter"></i> {/* X usa el ícono de Twitter */}
+            </a>
+
+            {/* Enlace a Discord */}
+            <a href="https://discord.com/invite/tu_invitacion" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-discord"></i>
+            </a>
           </div>
-
-          <div className='col-lg-3 col-md-12 enlacesFooter'>
-            
-            <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Guía de Compra</Link>
-            <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Medios de Pago</Link>
-            <Link className='linkFooter' to='/AtencionAlCliente'><i class="bi bi-diamond-fill icons"></i> Atención al Cliente</Link>
-            <Link className='linkFooter' to='/AtencionAlCliente'><i class="bi bi-diamond-fill icons"></i> Preguntas Frecuentes</Link>
-            <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Términos y Condiciones</Link>
-          </div>
-
-
-          
-
 
         </div>
 
+        <div className='col-lg-6 col-md-12 colCentralFooter'>
+          
+          <img className='planetaFooter' src={planeta} alt="" />  
+          
+        </div>
 
-
-
+        <div className='col-lg-3 col-md-12 enlacesFooter'>
+          
+          <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Guía de Compra</Link>
+          <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Medios de Pago</Link>
+          <Link className='linkFooter' to='/AtencionAlCliente'><i class="bi bi-diamond-fill icons"></i> Atención al Cliente</Link>
+          <Link className='linkFooter' to='/AtencionAlCliente'><i class="bi bi-diamond-fill icons"></i> Preguntas Frecuentes</Link>
+          <Link className='linkFooter' to='/GuiaCompra'><i class="bi bi-diamond-fill icons"></i> Términos y Condiciones</Link>
+        </div>
 
       </div>
-      )
-    }
+
+    </div>
+
+  )
+}
 
 
 export default Footer
