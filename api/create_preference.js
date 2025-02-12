1df6c24eef3bad84eab4d5cd098519cc8615546b
bosella <brunoosella08@gmail.com>
2025-02-12 17:03:01 -0300

    update preference

--- api/create_preference.js
+++ api/create_preference.js
@@ -8,33 +8,38 @@ const client = new MercadoPagoConfig({
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    try {
-      const body = {
-        items: [
-          {
-            title: req.body.title,
-            quantity: Number(req.body.quantity),
-            unit_price: Number(req.body.price),
-            currency_id: 'ARS',
-          },
-        ],
-        back_urls: {
-          success: 'https://www.youtube.com/watch?v=vEXwN9-tKcs&t=180s&ab_channel=onthecode',
-          failure: 'https://www.youtube.com/watch?v=vEXwN9-tKcs&t=180s&ab_channel=onthecode',
-          pending: 'https://www.youtube.com/watch?v=vEXwN9-tKcs&t=180s&ab_channel=onthecode',
-        },
-        auto_return: 'approved',
-        notification_url: import.meta.env.WEBHOOK_MP
-      };
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.price),
+          currency_id: "ARS"
+        }
+      ],
+      back_urls: {
+        success: "www.imperiotickets.com/Success",
+        failure: "https://www.youtube.com/watch?v=vEXwN9-tKcs&t=180s&ab_channel=onthecode",
+        pending: "https://www.youtube.com/watch?v=vEXwN9-tKcs&t=180s&ab_channel=onthecode"
+      },
+      auto_return: "approved",
+      notification_url: process.env.WEBHOOK_MP
+    };
 
-      const preference = new Preference(client);
-      const result = await preference.create({ body });
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
 
-      res.status(200).json({ id: result.id });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error al crear la preferencia' });
-    }
+    res.json({
+      id: result.id
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error al crear la preferencia"
+    });
+  }
   } else {
     res.status(405).json({ error: 'Método no permitido' });
   }
