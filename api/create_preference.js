af4dd1af07698d357959d799a25d97f7f9e7ce3e
bosella <brunoosella08@gmail.com>
2025-02-12 20:29:39 -0300

    Revert some changes

--- api/create_preference.js
+++ api/create_preference.js
@@ -13,13 +13,13 @@ const client = new MercadoPagoConfig({
 });
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://imperiotickets.com");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  // res.setHeader("Access-Control-Allow-Origin", "https://imperiotickets.com");
+  // res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  // res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end(); // Manejo de preflight request
-  }
+  // if (req.method === "OPTIONS") {
+  //   return res.status(200).end(); // Manejo de preflight request
+  // }
 
   if (req.method === 'POST') {
   try {
