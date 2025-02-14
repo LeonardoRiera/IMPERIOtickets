e9ccfeea754d70426e57227d4f6578f6fea321ce
bosella <brunoosella08@gmail.com>
2025-02-13 21:17:33 -0300

    ultimo fix?

--- api/webhook.js
+++ api/webhook.js
@@ -4,28 +4,21 @@ import fetch from "node-fetch";
 import nodemailer from 'nodemailer';
 import { Buffer } from 'buffer';
 import { jsPDF } from 'jspdf';
-import fs from "fs"
 import QRCode from 'qrcode';
-
 
 const app = express();
 app.use(express.json());
 
 export default async function handler(req, res) {
-// Configuración de Nodemailer con contraseña de aplicación
-const transporter = nodemailer.createTransport({
+  // Configuración de Nodemailer
+  const transporter = nodemailer.createTransport({
     service: 'Gmail', 
     auth: {
       user: 'imperiotickets@gmail.com', 
       pass: 'kpui dbjk dubd ljuj' 
     }
   });
-  
-  const toBase64 = (filePath) => {
-    const image = fs.readFileSync(filePath);
-    return `data:image/png;base64,${image.toString("base64")}`;
-  };
-  
+
   // Función para generar el QR en base64
   const generateQRCodeBase64 = async (id) => {
     try {
@@ -36,30 +29,26 @@ const transporter = nodemailer.createTransport({
       throw error;
     }
   };
-  
+
   // Función para generar el PDF con el QR
   const generatePDFWithQR = (qrBase64) => {
     const pdf = new jsPDF({
-      orientation:'portait',
+      orientation: 'portrait',
       unit: 'mm',
-      format:[100, 150]
+      format: [100, 150]
     });
-  
-    const logoUrl = toBase64('../IMPERIOtickets/src/assets/imagologoTickets.png')
-    // const logoUrl = toBase64('https://imperiotickets.com/assets/imagologoTickets-D6SFtBSe.png')
-    pdf.addImage(logoUrl, "PNG", 35, 10, 30, 30);
-  
+
     // Título del ticket
     pdf.setFont("helvetica", "bold");
     pdf.setFontSize(16);
     pdf.text("¡Tu entrada para el evento!", 50, 50, { align: "center" });
-  
+
     // Información del evento
     pdf.setFontSize(12);
     pdf.text("Fecha: 15 de Febrero 2025", 10, 70);
     pdf.text("Hora: 20:00", 10, 80);
     pdf.text("Ubicación: Teatro Central", 10, 90);
-  
+
     pdf.addImage(qrBase64, "PNG", 30, 100, 40, 40);
     
     const pdfBuffer = Buffer.from(pdf.output('arraybuffer'));
@@ -99,7 +88,7 @@ const transporter = nodemailer.createTransport({
             from: "imperiotickets@gmail.com",
             to: "brunoosella08@gmail.com",
             subject: "Entradas adjuntas",
-            text: "ESTÁN LISTAS TUS ENTADAS",
+            text: "ESTÁN LISTAS TUS ENTRADAS",
             attachments: mailAttachments,
           };
 
