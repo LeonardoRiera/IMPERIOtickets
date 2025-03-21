e1e86b33e8ceda28856c5b7e40bc62c118ceb6e9
bosella <brunoosella08@gmail.com>
2025-03-20 22:41:18 -0300

    fix stupid things

--- src/app/layout.js
+++ src/app/layout.js
@@ -42,7 +42,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} body`}>
         <div>
 
         <Header />
